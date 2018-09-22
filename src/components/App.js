9198108dca676e76c4c83141c27ec0b5324fb351
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-09-22 16:28:19 -0400

    #popup-layout - added DisplayComponent with button

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
 import Header from './header/Header'
+import DisplaySponsored from './display-sponsored/DisplaySponsored';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <DisplaySponsored />
       </div>
     );
   }
---
+++ src/components/display-sponsored/DisplaySponsored.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './DisplaySponsored.css'
+
+const DisplaySponsored = () => {
+  return (
+    <div id="display-sponsored">
+      <button type="button" id="display-button">Click</button>
+      <h4>Display Sponsored Content</h4>
+    </div>
+  )
+}
+
+export default DisplaySponsored
